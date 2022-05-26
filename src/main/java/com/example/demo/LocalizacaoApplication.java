ed22d9f4e9a7a973c712c4f7eb098b4b15c08fd2
paulo-pay <paulo.fernandes@payshopx.com.br>
2022-05-26 20:38:45 -0300

    feat: altearndo properties para .yml. Automatizando script das tabelas e configurando log do hebernate no console da IDE

--- src/main/java/com/example/demo/LocalizacaoApplication.java
+++ src/main/java/com/example/demo/LocalizacaoApplication.java
@@ -1,17 +1,46 @@
 package com.example.demo;
 
+import com.example.demo.domain.entity.Cidade;
+import com.example.demo.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CidadeRepository cidaderepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Inicializando projeto!!!");
+		//listarCidades();
+		//listarCidadesPorNome();
+		listarCidadesPorHabitantes();
+		System.out.println("");
 	}
 
+	void listarCidadesPorNome() {
+		cidaderepository.findByNome("Porto Velho").forEach(System.out::println);
+	}
+
+	void listarCidadesPorHabitantes() {
+		cidaderepository.findByHabitantes(4948989L).forEach(System.out::println);
+	}
+
+	@Transactional
+	void salvarCidade() {
+		var cidade = new Cidade(1L, "São Paulo", 12396372L);
+		cidaderepository.save(cidade);
+	}
+
+	void listarCidades() {
+		cidaderepository.findAll().forEach(System.out::println);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
 	}
--- src/main/java/com/example/demo/domain/entity/Cidade.java
+++ src/main/java/com/example/demo/domain/entity/Cidade.java
@@ -1,5 +1,9 @@
 package com.example.demo.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_cidade")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cidade {
 
     @Id
